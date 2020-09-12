3d6d7bff61704e4bd6465e5254023545c5a66914
ojeongmuo <70701413+ojeongmuo@users.noreply.github.com>
2020-09-13 00:46:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1361,7 +1361,7 @@ if(message.content == 'o!이스터에그') {
       .addBlankField()
 
       .addField('```탱커 대기중```', '```탱커 값 대기중```', true)
-      .addField('```딜러```', '```겐지,맥크리,위도우,한조,애쉬```')
+      .addField('```딜러```', '```모든 딜러 꿀값 완료```')
       .addField('```힐러 대기중```', '```힐러 값 대기중```', true)
       .setFooter('Kanto BOT❤️', img)
 
