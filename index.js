0fc0eb579724e82075151d4c3650f5e275c5f931
ojeongmuo <70701413+ojeongmuo@users.noreply.github.com>
2020-09-12 18:24:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1054,8 +1054,8 @@ if(message.content == 'o!솔저') {
       .setAuthor('영웅 이름', img, 'https://cdn.discordapp.com/attachments/726255182484209756/733620919565549619/4a8ffeacdbf6488d.gif')
       .setThumbnail(img)
       .addBlankField()
-      .addField('```딜러 영웅 목록```', '```diff
-/+겐지 한조 위도우 맥```')
+      .addField('```diff/딜러 영웅 목록```', '```diff/
++겐지 한조 위도우 맥```')
       .addField('Inline field title', 'Some value here', true)
       .addField('Inline field title', 'Some value here', true)
       .addField('Inline field title', 'Some value here', true)
