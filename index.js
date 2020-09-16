1a433016a7913a281634c33a0e392e67de521cd2
ojeongmuo <70701413+ojeongmuo@users.noreply.github.com>
2020-09-16 22:02:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -423,137 +423,149 @@ if(message.content == 'k!위도') {
     message.channel.send(embed)
   }
 
-    if message.content.startswith("ch!크힛 맥크리"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 맥크리값```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/680260392697724943/742365462847029248/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 위도우"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 위도우값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/680260392697724943/742366145100644352/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 애쉬"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 애쉬값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/680260392697724943/742366427675230309/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 겐지"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 겐지값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/680260392697724943/742366610936823818/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 둠피"):
-        embed = discord.Embed(title="Setting", description="**```fix\n앱큐님의 크힛 스무스 둠피값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/680260392697724943/721231852039569448/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 한조") :
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 한조값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/680260392697724943/742369648493985902/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 트레이서"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 트레이서값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/680260392697724943/742368716490145823/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 자리야"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 자리야값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/686401479040303150/743293031486062722/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 아나"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 아나값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/686401479040303150/743296522787094538/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 브리기테"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 브리기테값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745822809934004314/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 디바"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 디바값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745822981854462007/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 바티"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 바티값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745823246259060787/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 루시우"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 루시우값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745823311128297552/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 젠야타"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 젠야타값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745823407073001562/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 에코"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 에코값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745823795784188064/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 메르시"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 메르시값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745823840726417498/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 모이라"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 모이라값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745823840726417498/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 레킹볼"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 레킹볼값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745823916567691305/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 바스"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 바스티온값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745825054855528451/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 정크"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 정크렛값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745825054855528451/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 메이"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 메이값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745825343855656970/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 파라"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 파라값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745825449703112774/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 솔저"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 솔저값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745825660861415484/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 솜브라"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 솜브라값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745825791354339418/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 시메"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 시메트라값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745825939694419998/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
-    if message.content.startswith("ch!크힛 토르"):
-        embed = discord.Embed(title="Setting", description="**```fix\n아리님의 크힛 스무스 토르비욘값\n```**", color=0xff0000)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/681471048688074827/745825939694419998/unknown.png")
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/680260392697724943/742393147719417866/loading.gif")
-        await message.channel.send(embed=embed)
+if(message.content == 'ch!자리야') {
+    let img = 'https://cdn.discordapp.com/avatars/677970556473311284/a_fb1ded4a17c483fd50262a0531e05b9b.gif?size=512';
+    let embed = new Discord.RichEmbed()
+      .setURL('http://www.naver.com')
+      .setColor('#ff0000')
+      .setAuthor('자리야', img, )
+      .addField('```Name 자리야```', '```Good```', true)
+      .setThumbnail(img)
+      .setImage('https://cdn.discordapp.com/attachments/755758873335300320/755758893619216515/c89ddb9bde2df3b0.png')
+      .setFooter('CH BOT❤️', img)
+    message.channel.send(embed)
+  }
 
+if(message.content == 'ch!맥크리') {
+    let img = 'https://cdn.discordapp.com/avatars/677970556473311284/a_fb1ded4a17c483fd50262a0531e05b9b.gif?size=512';
+    let embed = new Discord.RichEmbed()
+      .setURL('http://www.naver.com')
+      .setColor('#ff0000')
+      .setAuthor('맥', img, )
+      .addField('```Name 맥```', '```.```', true)
+      .setThumbnail(img)
+      .setImage('https://cdn.discordapp.com/attachments/755758873335300320/755759302291095633/f802a0aa5a355272.png')
+      .setFooter('CH BOT❤️', img)
+    message.channel.send(embed)
+  }
+
+if(message.content == 'ch!맥') {
+    let img = 'https://cdn.discordapp.com/avatars/677970556473311284/a_fb1ded4a17c483fd50262a0531e05b9b.gif?size=512';
+    let embed = new Discord.RichEmbed()
+      .setURL('http://www.naver.com')
+      .setColor('#ff0000')
+      .setAuthor('맥', img, )
+      .addField('```Name 맥```', '```.```', true)
+      .setThumbnail(img)
+      .setImage('https://cdn.discordapp.com/attachments/755758873335300320/755759302291095633/f802a0aa5a355272.png')
+      .setFooter('CH BOT❤️', img)
+    message.channel.send(embed)
+  }
+
+if(message.content == 'ch!위도우') {
+    let img = 'https://cdn.discordapp.com/avatars/677970556473311284/a_fb1ded4a17c483fd50262a0531e05b9b.gif?size=512';
+    let embed = new Discord.RichEmbed()
+      .setURL('http://www.naver.com')
+      .setColor('#ff0000')
+      .setAuthor('위도우', img, )
+      .addField('```Name Widow```', '```good```', true)
+      .setThumbnail(img)
+      .setImage('https://cdn.discordapp.com/attachments/755758873335300320/755759550048501760/e651d25c74095527.png')
+      .setFooter('CH BOT❤️', img)
+    message.channel.send(embed)
+  }
+
+if(message.content == 'ch!바티') {
+    let img = 'https://cdn.discordapp.com/avatars/677970556473311284/a_fb1ded4a17c483fd50262a0531e05b9b.gif?size=512';
+    let embed = new Discord.RichEmbed()
+      .setURL('http://www.naver.com')
+      .setColor('#ff0000')
+      .setAuthor('바티', img, )
+      .addField('```Name Baptiste```', '```good```', true)
+      .setThumbnail(img)
+      .setImage('https://cdn.discordapp.com/attachments/755758873335300320/755759550048501760/e651d25c74095527.png')
+      .setFooter('CH BOT❤️', img)
+    message.channel.send(embed)
+  }
+
+if(message.content == 'ch!바티스트') {
+    let img = 'https://cdn.discordapp.com/avatars/677970556473311284/a_fb1ded4a17c483fd50262a0531e05b9b.gif?size=512';
+    let embed = new Discord.RichEmbed()
+      .setURL('http://www.naver.com')
+      .setColor('#ff0000')
+      .setAuthor('바티스트', img, )
+      .addField('```Name Baptiste```', '```good```', true)
+      .setThumbnail(img)
+      .setImage('https://cdn.discordapp.com/attachments/755758873335300320/755759550048501760/e651d25c74095527.png')
+      .setFooter('CH BOT❤️', img)
+    message.channel.send(embed)
+  }
+
+if(message.content == 'ch!위도') {
+    let img = 'https://cdn.discordapp.com/avatars/677970556473311284/a_fb1ded4a17c483fd50262a0531e05b9b.gif?size=512';
+    let embed = new Discord.RichEmbed()
+      .setURL('http://www.naver.com')
+      .setColor('#ff0000')
+      .setAuthor('위도', img, )
+      .addField('```Name Widow```', '```good```', true)
+      .setThumbnail(img)
+      .setImage('https://cdn.discordapp.com/attachments/755758873335300320/755759550048501760/e651d25c74095527.png')
+      .setFooter('CH BOT❤️', img)
+    message.channel.send(embed)
+  }
+
+if(message.content == 'ch!위도') {
+    let img = 'https://cdn.discordapp.com/avatars/677970556473311284/a_fb1ded4a17c483fd50262a0531e05b9b.gif?size=512';
+    let embed = new Discord.RichEmbed()
+      .setURL('http://www.naver.com')
+      .setColor('#ff0000')
+      .setAuthor('위도', img, )
+      .addField('```Name 위도```', '```.```', true)
+      .setThumbnail(img)
+      .setImage('https://cdn.discordapp.com/attachments/755758873335300320/755759550048501760/e651d25c74095527.png')
+      .setFooter('CH BOT❤️', img)
+    message.channel.send(embed)
+  }
+if(message.content == 'ch!애쉬') {
+    let img = 'https://cdn.discordapp.com/avatars/677970556473311284/a_fb1ded4a17c483fd50262a0531e05b9b.gif?size=512';
+    let embed = new Discord.RichEmbed()
+      .setURL('http://www.naver.com')
+      .setColor('#ff0000')
+      .setAuthor('애쉬', img, )
+      .addField('```Name Ash```', '```.```', true)
+      .setThumbnail(img)
+      .setImage('https://cdn.discordapp.com/attachments/755758873335300320/755759910079430696/f0ed579e92c20010.png')
+      .setFooter('CH BOT❤️', img)
+    message.channel.send(embed)
+  }
+
+if(message.content == 'ch!겐지') {
+    let img = 'https://cdn.discordapp.com/avatars/677970556473311284/a_fb1ded4a17c483fd50262a0531e05b9b.gif?size=512';
+    let embed = new Discord.RichEmbed()
+      .setURL('http://www.naver.com')
+      .setColor('#ff0000')
+      .setAuthor('겐지', img, )
+      .addField('```Name Genji```', '```Good```', true)
+      .setThumbnail(img)
+      .setImage('https://cdn.discordapp.com/attachments/755758873335300320/755760181861941348/e9743acf491b35e1.png')
+      .setFooter('CH BOT❤️', img)
+    message.channel.send(embed)
+  }
+
+
+
+if(message.content == 'ch!아나') {
+    let img = 'https://cdn.discordapp.com/avatars/677970556473311284/a_fb1ded4a17c483fd50262a0531e05b9b.gif?size=512';
+    let embed = new Discord.RichEmbed()
+      .setURL('http://www.naver.com')
+      .setColor('#ff0000')
+      .setAuthor('아나', img, )
+      .addField('```Name Ana```', '```.```', true)
+      .setThumbnail(img)
+      .setImage('https://cdn.discordapp.com/attachments/755758873335300320/755761228940443668/d2e4f82f3771cebb.png')
+      .setFooter('CH BOT❤️', img)
+    message.channel.send(embed)
+  }
 
 
 
@@ -1504,7 +1516,7 @@ if(message.content == 'o!이스터에그') {
 
 else if(message.content == 'ch!help') 
        {
-        let helpImg = 'https://cdn.discordapp.com/attachments/726255182484209756/733620919565549619/4a8ffeacdbf6488d.gif';
+        let helpImg = 'https://cdn.discordapp.com/avatars/677970556473311284/a_fb1ded4a17c483fd50262a0531e05b9b.gif?size=512';
         let commandList = [
        {name: 'ch!(영웅이름)', desc: '입력한 영웅에 값을 불러옵니다'},
       {name: 'ch!영웅 목록', desc: '꿀값 있는 영웅들을 불러옵니다'},
@@ -1526,7 +1538,7 @@ else if(message.content == 'ch!help')
   }
 
 if(message.content == 'ch!영웅 목록') {
-    let img = 'https://cdn.discordapp.com/attachments/726255182484209756/733620919565549619/4a8ffeacdbf6488d.gif';
+    let img = 'https://cdn.discordapp.com/avatars/677970556473311284/a_fb1ded4a17c483fd50262a0531e05b9b.gif?size=512';
     let embed = new Discord.RichEmbed()
 .setColor('#ff0000')
 .addField('```영웅 목록```', '``Helo List``')
